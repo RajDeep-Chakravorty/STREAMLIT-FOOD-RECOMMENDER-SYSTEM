feabd2fba776940797a129110104981612b7dfd4
Rajdeep Chakravorty <98254554+RajDeep-Chakravorty@users.noreply.github.com>
2024-04-19 03:06:48 +0530

    Update app.py

--- Deploy/app.py
+++ Deploy/app.py
@@ -8,7 +8,7 @@ header_image = "https://github.com/RajDeep-Chakravorty/STREAMLIT-FOOD-RECOMMENDE
 st.image(header_image, use_column_width=True)
 st.title("Food Recommendation System")
 st.text("Let us help you with ordering")
-st.markdown("<img src='https://github.com/RajDeep-Chakravorty/STREAMLIT-FOOD-RECOMMENDER-SYSTEM/raw/main/Deploy/foood.jpg' width=300>", unsafe_allow_html=True)
+st.markdown("<img src='https://github.com/RajDeep-Chakravorty/STREAMLIT-FOOD-RECOMMENDER-SYSTEM/raw/main/Deploy/foood.jpg' width=600>", unsafe_allow_html=True)
 
 st.subheader("Whats your preference?")
 vegn = st.radio("Vegetables or none!", ["veg", "non-veg"], index=1) 
